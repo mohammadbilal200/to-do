13dcf2b1f6d0411f3ab705bf849675b752e6704b
mohammadbilal200 <mohammadbilal200@hotmail.com>
2023-01-01 00:51:20 +0500

    minor CSS and task insertion improved

--- script/script.js
+++ script/script.js
@@ -1,24 +1,25 @@
 let tasksList=[];
+
 addTask = () => {
     const task=document.getElementById("task-input").value;
-    const taskNumber=tasksList.length
-    tasksList.push('<button onclick="removeTask(event)" id=btn-'+taskNumber+'>'+(taskNumber+1)+'</button><ul>'+task+'</ul>');
-    document.getElementById("tasks").innerHTML=tasksList.join(`
-    `);
+    const taskNumber=tasksList.length/2;
+    tasksList.push('<button onclick="removeTask(event)" id=btn-'+taskNumber+'>'+(taskNumber+1)+'</button>');
+    tasksList.push('<ul>'+task+'</ul>');
+    display();
 }
+
 const removeTask = (event) => {
-    const taskNumber= event.target.id.slice(-1);
-    tasksList.splice(taskNumber, 1);
-    // tasksList.forEach(renumber);
+    const taskNumber= event.target.id.slice(-1)*2;
+    tasksList.splice(taskNumber, 2);
+    for(let i=0; i<=tasksList.length; i+=2){
+        tasksList[i].removeAttribute("id");
+        tasksList[i].setAttribute("id", "btn-"+(i/2));
+        tasksList[i].value=(i/2)+1;
+    }
+    // tasksList.forEach(reNumber);
+    display();
+}
+
+const display = () => {
     document.getElementById("tasks").innerHTML=tasksList.join(`
-    `);
-}
-
-
-
-const renumber = (item, index, arr) => {
-    item.removeAttribute("id");
-    item.setAttribute("id", "btn-"+index);
-    item.value=index+1;
-    arr[index] = item;
-}
+    `);}
